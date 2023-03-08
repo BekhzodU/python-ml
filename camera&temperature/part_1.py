c4739089c6d316b64a803479c50034202d32e7f7
Bekhzod i2max <bekhzod.ubaydullaev@i2max.co.kr>
2023-03-08 22:40:01 +0500

    ip webcam comments added

--- camera&temperature/part_1.py
+++ camera&temperature/part_1.py
@@ -1,12 +1,12 @@
 import cv2;
 
-# add an IP adress
+# add an IP adress that is shown in IP Webcam. For current desktop use value '0'
 capture = cv2.VideoCapture('http://192.168.100.11:8080/video');
 
 while True:
     ret, frame = capture.read(); #read frames in a loop
     if frame is not None:
-        resized = cv2.resize(frame, (800, 550)); #resize the frame
+        resized = cv2.resize(frame, (800, 550)); #resize the frame (desktop)
         cv2.imshow("Frame", resized); #display the frame on desktop
 
     key = cv2.waitKey(1); #to exit the frame press 'q'
