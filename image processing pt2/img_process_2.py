a2737a4c86b17b4f30d191f63a1aac516be97df0
Bekhzod i2max <bekhzod.ubaydullaev@i2max.co.kr>
2023-04-19 18:46:31 +0500

    rotation

--- image processing pt2/img_process_2.py
+++ image processing pt2/img_process_2.py
@@ -13,7 +13,18 @@ def translation(img):
     plt.yticks([])
     plt.show()
 
+def rotation(img):
+    height, width = img.shape[:2]
+    angle = 45
+    center = (width // 2, height // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 0.5)
+    rotated_img = cv2.warpAffine(img, M, (width, height), borderMode=cv2.BORDER_CONSTANT)
+
+    plt.imshow(rotated_img)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
 
 img = cv2.imread('newyork.jpeg')
 assert img is not None, "file could not be read"
-translation(img)
+rotation(img)
